aa30be6d6f43b30439792f5e0bd47cb72b212bc5
Aryan Ganotra <aryanganotra7@gmail.com>
2019-08-03 01:13:12 +0530

    .

--- app/src/main/java/com/attendance/letmeattend/MainActivity.kt
+++ app/src/main/java/com/attendance/letmeattend/MainActivity.kt
@@ -2,12 +2,16 @@ package com.attendance.letmeattend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.attendance.letmeattend.LoginScreen.FirebaseLogin
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val  firebaseLogin:FirebaseLogin
+
     }
 
     ///helloooooooooo

c09876a4ba183aeffaa5ea3cf0cd76b274a3a19a
Aryan Ganotra <aryanganotra7@gmail.com>
2019-12-06 06:38:28 +0530

    Update BindingAdapter.kt

--- app/src/main/java/com/attendance/letmeattend/utils/BindingAdapter.kt
+++ app/src/main/java/com/attendance/letmeattend/utils/BindingAdapter.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
+import com.attendance.letmeattend.adapters.LectureRecyclerAdapter
 import com.attendance.letmeattend.models.Lecture
 import com.attendance.letmeattend.utils.extentions.getParentActivity
 import com.google.android.material.tabs.TabLayout
@@ -18,8 +19,17 @@ fun setMutableTest(view: RecyclerView, lectures : MediatorLiveData<ArrayList<Lec
 
     val parentActivity: AppCompatActivity? = view.getParentActivity()
     if (parentActivity != null && lectures != null){
-       lectures.observe(parentActivity, Observer { value -> view.adapter?.setLectures(value):null })
+       lectures.observe(parentActivity, Observer { value -> view.adapter?.setLectures(value) })
     }
 
 
+}
+
+@BindingAdapter("setupAdapter")
+fun setAdapter(view: RecyclerView, adapter : LectureRecyclerAdapter)
+{
+    if (view!=null && adapter!=null)
+    {
+        view.adapter = adapter
+    }
 }
